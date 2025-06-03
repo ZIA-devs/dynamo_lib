2a902aebb9dfc980432ad5e2d6669c04de312caa
PP <ppaulo030601@gmail.com>
2025-06-03 12:10:49 -0300

    feat: Enhance logging and refactor import statements in BaseCrud class

--- dynamo_lib/cruds/_base_crud.py
+++ dynamo_lib/cruds/_base_crud.py
@@ -3,8 +3,10 @@ from typing import TypeVar, Generic, Type, Any, Dict, Optional, ClassVar
 from pydantic import BaseModel
 from enum import Enum
 
-import time
-import random
+from time import time
+from random import getrandbits
+from loguru import logger
+
 
 T = TypeVar("T", bound=BaseModel)
 
@@ -12,36 +14,41 @@ class BaseCrud(Generic[T]):
     TABLE_NAME: ClassVar[TableName] = 'Companies'
     SK_MARKER: ClassVar[str] = ""
     model: Type[T]
+    debug: ClassVar[bool] = False
 
 
     @classmethod
     def get_new_id(cls) -> str:
-        timestamp_ms = int(time.time() * 1000)
-        random_bits = random.getrandbits(12)
+        timestamp_ms = int(time() * 1000)
+        random_bits = getrandbits(12)
         return f"{timestamp_ms << 12 | random_bits}"
 
 
     @classmethod
     def scan(cls, filter_expression: str | None = None) -> list[T]:
         items = dynamo_scan(table_name=cls.TABLE_NAME, filter_expression=filter_expression)
+        if cls.debug: logger.info(f"Scanned items: {items}")
         return [cls.model(**item) for item in items if isinstance(item, dict)]
 
 
     @classmethod
     def list(cls, pk: str|int, sk: str|int = '') -> list[T]:
         items = dynamo_query(pk=str(pk), sk=cls.SK_MARKER + str(sk), table_name=cls.TABLE_NAME)
+        if cls.debug: logger.info(f"Queried items for pk={pk}, sk={sk}: {items}")
         return [cls.model(**item) for item in items]
 
 
     @classmethod
     def get(cls, pk: str|int, sk: str|int = "") -> Optional[T]:
         result = dynamo_query(pk=str(pk), sk=cls.SK_MARKER + str(sk), table_name=cls.TABLE_NAME)
+        if cls.debug: logger.info(f"Queried item for pk={pk}, sk={sk}: {result}")
         return cls.model(**result[0]) if result else None
 
 
     @classmethod
     def add(cls, pk: str|int, sk: Optional[str|int], data:Dict[str, Any]) -> T:
         sk_text = cls.SK_MARKER + str(sk) if sk is not None else None
+        if cls.debug: logger.info(f"Adding item with pk={pk}, sk={sk_text}, data={data}")
         if not dynamo_create(pk=str(pk), sk=sk_text, item=data, table_name=cls.TABLE_NAME):
             raise Exception("Error adding item to DynamoDB")
         return cls.model(**data)
@@ -52,6 +59,7 @@ class BaseCrud(Generic[T]):
         item_id = cls.get_new_id()
         data[id_key] = item_id
         sk = f'{sk}#{item_id}' if sk else str(item_id)
+        if cls.debug: logger.info(f"Adding item with pk={pk}, sk={sk}, data={data}")
         if not dynamo_create(pk=str(pk), sk=cls.SK_MARKER + sk, item=data, table_name=cls.TABLE_NAME):
             raise Exception("Error adding item to DynamoDB")
         return cls.model(**data)
@@ -59,12 +67,12 @@ class BaseCrud(Generic[T]):
 
     @classmethod
     def delete(cls, pk: str|int, sk: str|int = "") -> bool:
+        if cls.debug: logger.info(f"Deleting item with pk={pk}, sk={cls.SK_MARKER + str(sk)}")
         return dynamo_delete(pk=str(pk), sk=cls.SK_MARKER + str(sk), table_name=cls.TABLE_NAME)
 
 
     @classmethod
     def update(cls, pk: str|int, sk: str|int ="", **kwargs) -> bool:
-        print(kwargs)
         allowed_fields = {
             field_name: field.alias
             for field_name, field in cls.model.model_fields.items()
@@ -74,6 +82,7 @@ class BaseCrud(Generic[T]):
         for k, v in kwargs.items():
             if k not in allowed_fields: raise ValueError(f"Field '{k}' is not allowed for update.")
             update_values[allowed_fields[k]] = v.value if isinstance(v, Enum) else v
-
+            
+        if cls.debug: logger.info(f"Updating item with pk={pk}, sk={cls.SK_MARKER + str(sk)}, update_values={update_values}")
         if not update_values: return False
         return dynamo_update(pk=str(pk), sk=cls.SK_MARKER + str(sk), update_values=update_values, table_name=cls.TABLE_NAME)
