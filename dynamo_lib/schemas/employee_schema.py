24c0d8d8a52d10006093feb734c2000d06368f3b
PP <ppaulo030601@gmail.com>
2025-06-04 15:50:59 -0300

    feat: Add field validator for token_google_calendar in EmployeeSchema

--- dynamo_lib/schemas/employee_schema.py
+++ dynamo_lib/schemas/employee_schema.py
@@ -1,6 +1,7 @@
 from .company_shift_schema import CompanyShiftSchema
-from pydantic import Field
-from typing import Any
+from pydantic import Field, field_validator
+from typing import Any, Dict
+import json
 
 
 class EmployeeSchema(CompanyShiftSchema):
@@ -9,3 +10,8 @@ class EmployeeSchema(CompanyShiftSchema):
     id: int = Field(default=0, alias='employee_id', description='ID of the employee')
     name: str = Field(default='', alias='employee_name', description='Name of the employee')
     services:list[int] = Field(default_factory=list, alias='employee_services', description='List of service IDs offered by the employee')
+
+    @field_validator('token_google_calendar', mode='before')
+    @classmethod
+    def convert_token_google_calendar_to_dict(cls, v: str|Dict[str, Any]) -> Dict[str, Any]: 
+        return json.loads(v) if isinstance(v, str) else v
