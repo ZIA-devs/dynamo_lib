4f797b373204eff4876b4b3133a812ba14e25cd3
PP <ppaulo030601@gmail.com>
2025-06-04 11:39:25 -0300

    fix: Update type hints for token_google_calendar and phone_id in ConfigsSchema

--- dynamo_lib/schemas/configs_schema.py
+++ dynamo_lib/schemas/configs_schema.py
@@ -1,6 +1,7 @@
 from ..core.enums import EmpresaPlan
 from pydantic import BaseModel, Field, field_validator
-from typing import Any
+from typing import Any, Dict
+import json
 
 
 class ConfigsSchema(BaseModel):
@@ -11,7 +12,7 @@ class ConfigsSchema(BaseModel):
     company_name: str = Field(default='', alias='company_name', description='Name of the company')
     employee_limit: int = Field(default=99, alias='employee_limit', description='Limit of employees')
     plan: EmpresaPlan = Field(default=EmpresaPlan.SCHEDULE, alias='plan', description='Plan of the company')
-    token_google_calendar: dict[str, Any] = Field(default_factory=dict, alias='token_google_calendar', description='Google Calendar token')
+    token_google_calendar: Dict[str, Any] = Field(default_factory=dict, alias='token_google_calendar', description='Google Calendar token')
     token_asa: str = Field(default='', alias='token_asa', description='ASA token')
     token_openai: str = Field(default='', alias='token_openai', description='OpenAI token')
     token_wpp: str = Field(default='', alias='token_wpp', description='WhatsApp token')
@@ -19,4 +20,10 @@ class ConfigsSchema(BaseModel):
 
     @field_validator('phone_id', mode='before')
     @classmethod
-    def convert_phone_id_to_str(cls, v: int) -> str: return str(v)
+    def convert_phone_id_to_str(cls, v: int|str) -> str: return str(v)
+
+
+    @field_validator('token_google_calendar', mode='before')
+    @classmethod
+    def convert_token_google_calendar_to_dict(cls, v: str|Dict[str, Any]) -> Dict[str, Any]: 
+        return json.loads(v) if isinstance(v, str) else v
