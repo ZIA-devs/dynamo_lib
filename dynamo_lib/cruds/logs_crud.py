5ed6f9440d669c88b3e05d71d670bb994852e295
PP <ppaulo030601@gmail.com>
2025-06-05 16:36:14 -0300

    fix: Refactor get_msg method to return list of messages and update create method for bulk logging

--- dynamo_lib/cruds/logs_crud.py
+++ dynamo_lib/cruds/logs_crud.py
@@ -1,9 +1,10 @@
 from ..schemas import LogsSchema, ConfigsSchema
 from .reengagement_crud import ReengagementCrud
 from ._base_crud import BaseCrud
-from typing import Dict, Any
+from typing import Dict, List, Any
 from datetime import datetime, timedelta
-import pytz 
+import pytz
+import json
 
 
 timezone = pytz.timezone('America/Sao_Paulo')
@@ -12,37 +13,42 @@ class LogsCrud(BaseCrud[LogsSchema]):
     model = LogsSchema
 
 
-    def get_msg(self, message_type: str, message_body: str | Dict[str, Any], company_config: ConfigsSchema) -> str:
-        if message_type == 'text': return str(message_body)
+    def get_msg(self, message_type: str, message_body: str, company_config: ConfigsSchema) -> List[str]:
+        if message_type == 'text': return [message_body]
         if message_type == 'template' and isinstance(message_body, dict):
             template_name = message_body.get('name', '')
             match template_name:
-                case 'lembrete_agendamento': return "{mensagem de lembrete}"
-                case name if "agendamento" in name:  return "{mensagem do tipo formulario}"
+                case 'lembrete_agendamento': return ["{mensagem de lembrete}"]
+                case name if "agendamento" in name:  return ["{mensagem do tipo formulario}"]
                 case 'reengajamento':
                     reengagement = ReengagementCrud.get(company_config.phone_id)
-                    if reengagement: return f"*{company_config.assistant_name}:*\n\n{reengagement.message}"
-                    return "{mensagem de reengajamento}"
-                case 'cliente_interagiu': return "{mensagem de aviso de interação}"
-                case _: return "{mensagem de template}"
+                    if reengagement: return [f"*{company_config.assistant_name}:*\n\n{reengagement.message}"]
+                    return ["{mensagem de reengajamento}"]
+                case 'cliente_interagiu': return ["{mensagem de aviso de interação}"]
+                case _: return ["{mensagem de template}"]
 
-        if message_type in {'interactive', 'interactive_list'}: return "{interactive}"
-        return f'{{{message_type}}}=>{message_body}'
+        if message_type in {'interactive', 'interactive_list'}: return ["{interactive}"]
+
+        if message_type == 'contacts':
+            return [f"{{contact}}=>{json.dumps(contact)}" for contact in json.loads(message_body)]
+        
+        return [f'{{{message_type}}}=>{message_body}']
 
 
     @classmethod
-    def create(cls, company_config: ConfigsSchema, client_phone: str, message_body: str | Dict[str, Any], message_type: str, user_type:int) -> LogsSchema:
-        msg = cls().get_msg(message_type, message_body, company_config)
+    def create(cls, company_config: ConfigsSchema, client_phone: str, message_body: str, message_type: str, user_type:int) -> None:
+        msgs = cls().get_msg(message_type, message_body, company_config)
         sender = 'user' if user_type == 0 else 'assistant' if user_type == 1 else 'setor'
         timestamp = datetime.now(timezone).isoformat()
 
-        data = {
-            'client_id': client_phone,
-            'message': msg,
-            'sender': sender,
-            'type': message_type,
-            'time': timestamp,
-            'ttl': int((datetime.now() + timedelta(days=90)).timestamp()),
-        }  
-        log_sk = f"{client_phone}#{timestamp}"
-        return super().add(pk=company_config.phone_id, sk=log_sk, data=data)
+        for msg in msgs:
+            data = {
+                'client_id': client_phone,
+                'message': msg,
+                'sender': sender,
+                'type': message_type,
+                'time': timestamp,
+                'ttl': int((datetime.now() + timedelta(days=90)).timestamp()),
+            }  
+            log_sk = f"{client_phone}#{timestamp}"
+            super().add(pk=company_config.phone_id, sk=log_sk, data=data)
