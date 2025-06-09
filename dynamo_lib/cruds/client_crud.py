b1c5d5b957d263fb8484d0ce10cc6706e7f2b399
PP <ppaulo030601@gmail.com>
2025-06-09 13:44:47 -0300

    fix: Implement create method in ClientCrud for adding new clients

--- dynamo_lib/cruds/client_crud.py
+++ dynamo_lib/cruds/client_crud.py
@@ -6,3 +6,11 @@ class ClientCrud(BaseCrud[ClientSchema]):
     SK_MARKER = "client#"
     model = ClientSchema
     
+    @classmethod
+    def create(cls, phone_id: str, name: str, client_id: str|int, last_message:str="") -> ClientSchema:
+        client_data = {
+            'client_name': name,
+            'client_last_message': last_message
+        }
+        return super().add(phone_id, client_id, client_data)
+    
