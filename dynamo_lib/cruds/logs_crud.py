85a07c5f063f61907c77d6a6eda51237dcfbfe04
PP <ppaulo030601@gmail.com>
2025-08-18 11:22:35 -0300

    fix: update message_origin to use the enum directly in LogsCrud.create method

--- dynamo_lib/cruds/logs_crud.py
+++ dynamo_lib/cruds/logs_crud.py
@@ -89,7 +89,7 @@ class LogsCrud(BaseCrud[LogsSchema]):
                 "type": message_type,
                 "time": timestamp,
                 "ttl": int((datetime.now() + timedelta(days=90)).timestamp()),
-                "message_origin": message_origin.value,
+                "message_origin": message_origin,
             }
             log_sk = f"{client_phone}#{timestamp}"
             super().add(pk=company_config.phone_id, sk=log_sk, data=data)
