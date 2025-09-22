65e6c3859f12af433fc29aea81c11bad5c417006
PP <ppaulo030601@gmail.com>
2025-09-22 12:21:39 -0300

    refactor: simplify CheckoutSchema by removing CouponSchema and adjusting coupons field to use a dictionary of floats

--- dynamo_lib/schemas/checkout.py
+++ dynamo_lib/schemas/checkout.py
@@ -1,10 +1,5 @@
-from pydantic import BaseModel, Field, field_validator
-from typing import Dict, List
-
-
-class CouponSchema(BaseModel):
-    code: str = Field(alias="code", description="Cupon code")
-    discount: float = Field(alias="discount", description="Discount percentage")
+from pydantic import BaseModel, Field
+from typing import Dict
 
 
 class CheckoutSchema(BaseModel):
@@ -14,16 +9,8 @@ class CheckoutSchema(BaseModel):
     description: str = Field(
         default="", alias="description", description="Description of the item"
     )
-    coupons: Dict[str, CouponSchema] = Field(
-        default=[],
+    coupons: Dict[str, float] = Field(
+        default_factory=dict,
         alias="coupons",
         description="List of coupons associated with the item",
     )
-
-    @field_validator("coupons", mode="before")
-    def validate_coupons(cls, value: List | None) -> Dict[str, CouponSchema]:
-        if value is None:
-            return {}
-        if isinstance(value, list):
-            return {coupon["code"]: CouponSchema(**coupon) for coupon in value}
-        return value
